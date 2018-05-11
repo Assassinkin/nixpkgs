24a68fbc0091772c70d5e36de9cf9b8190afe2b2
Assassinkin <iuns@outlook.fr>
2018-05-11 17:20:06 +0100

    Fix indentation

--- pkgs/development/python-modules/mail-parser/default.nix
+++ pkgs/development/python-modules/mail-parser/default.nix
@@ -5,15 +5,15 @@ buildPythonPackage rec {
   version = "3.3.1";
 
   src = fetchPypi {
-     inherit pname version;
-     sha256 = "0w8hwcld67j6hqzjycbbqk6pvsjpg0a5si6fzzdhzdh7h55y6gd9";
-   };
+    inherit pname version;
+    sha256 = "0w8hwcld67j6hqzjycbbqk6pvsjpg0a5si6fzzdhzdh7h55y6gd9";
+  };
 
   propagatedBuildInputs = [ ipaddress six simplejson ];
 
   meta = with stdenv.lib; {
-     description = "A mail parser for python 2 and 3";
-     homepage = https://github.com/SpamScope/mail-parser;
-     license = licenses.asl20;
+    description = "A mail parser for python 2 and 3";
+    homepage = https://github.com/SpamScope/mail-parser;
+    license = licenses.asl20;
   };
 }
