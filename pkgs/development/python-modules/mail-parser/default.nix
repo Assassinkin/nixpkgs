7521ee7b28dcdd6f0c4152c62b02184ac82807ca
Assassinkin <iuns@outlook.fr>
2018-05-11 15:30:26 +0100

    Fix indentation and enabling checks

--- pkgs/development/python-modules/mail-parser/default.nix
+++ pkgs/development/python-modules/mail-parser/default.nix
@@ -1,22 +1,20 @@
 { stdenv, pkgs, buildPythonPackage, fetchPypi, ipaddress, six, simplejson }:
 
 buildPythonPackage rec {
- pname = "mail-parser";
- version = "3.3.1";
+  pname = "mail-parser";
+  version = "3.3.1";
 
-   src = fetchPypi {
+  src = fetchPypi {
      inherit pname version;
      sha256 = "0w8hwcld67j6hqzjycbbqk6pvsjpg0a5si6fzzdhzdh7h55y6gd9";
    };
 
-   propagatedBuildInputs = [ ipaddress six simplejson ];
+  propagatedBuildInputs = [ ipaddress six simplejson ];
 
-   doCheck = false;
-
-   meta = with stdenv.lib; {
+  meta = with stdenv.lib; {
      description = "A mail parser for python 2 and 3";
      homepage = https://github.com/SpamScope/mail-parser;
      license = licenses.asl20;
      maintainers = with maintainers; [ Assassinkin ];
-   };
+  };
 }
